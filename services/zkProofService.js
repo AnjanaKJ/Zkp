1091b5e1cbd15d438933f44c1e4137f4d79a38b0
Anjana Kj <kjanjana0081@gmail.com>
2024-11-18 09:37:53 +0530

    fix: circom path

--- services/zkProofService.js
+++ services/zkProofService.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 
 exports.generateProof = async (ehrData) => {
   try {
-    const wasmPath = '../Zkp/circom/circom/ehr_proof_js/ehr_proof.wasm';
-    const zkeyPath = '../Zkp/circom/circom/ehr_proof.zkey';
+    const wasmPath = '../circom/circom/ehr_proof_js/ehr_proof.wasm';
+    const zkeyPath = '../circom/circom/ehr_proof.zkey';
     console.log("Loading WASM...");
     const { proof, publicSignals } = await snarkjs.groth16.fullProve(
        ehrData,
