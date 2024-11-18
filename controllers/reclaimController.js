19f8bc4a399a08eb79d31a5ee34d22b45915cf06
Anjana Kj <kjanjana0081@gmail.com>
2024-11-18 09:28:31 +0530

    fix: model name

--- controllers/reclaimController.js
+++ controllers/reclaimController.js
@@ -1,4 +1,4 @@
-const User = require('../models/user');
+const User = require('../models/User');
 const { generateProof } = require('../services/zkProofService');
 
 exports.receiveEHR = async (req, res) => {
